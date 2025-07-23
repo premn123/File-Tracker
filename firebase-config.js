68d66a4e3c55c12d686de05eb18ee9e0e9013c16
premnarayan <premn1429@gmail.com>
2025-07-24 05:24:02 +0530

    Vite setup with Firebase config via .env

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// firebase-config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyB_ldtBhJvkYmspTXrgGhcdXODkhA-Rlvo",
-  authDomain: "file-tracker-377f8.firebaseapp.com",
-  databaseURL: "https://file-tracker-377f8-default-rtdb.firebaseio.com",
-  projectId: "file-tracker-377f8",
-  storageBucket: "file-tracker-377f8.appspot.com",
-  messagingSenderId: "519361063057",
-  appId: "1:519361063057:web:926202c9d3aa94166a7730"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.', // index.html ka path
+  server: {
+    port: 3000, // optional
+  }
+});
