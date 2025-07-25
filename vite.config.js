554e26a43d0234193f67d0dcfda0639b15dd147a
premnarayan <premn1429@gmail.com>
2025-07-25 07:53:17 +0530

    Initial commit - all files as it is

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: '.', // index.html ka path
-  server: {
-    port: 3000, // optional
-  }
-});
+export default {
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
+  publicDir: '../public'
+};
